4dde4886ee0dd10dee72627dafaf177192c2d0dc
NanaYaa111 <noobuobie@st.ug.edu.gh>
2025-12-10 16:22:37 +0000

    First commit

---
+++ python/trial.py
@@ -0,0 +1,14 @@
+playlist=["Memories","Friends","Cool for the Summer", "Lonely", "Midnight City"]
+
+top_artists=("Maroon 5","Dua Lipa","Ariana Grande","Billie Eilish")
+
+my_music=[playlist, top_artists]
+
+print(my_music)
+
+print(playlist[2])
+
+playlist[0]="Shape of You"
+
+playlist.append("Blinding Lights")
+print(playlist)
